2ddd52a6a4d884f090961c02966bc76e7359d297
Hamas Akhtar <107211584+HamasAtr@users.noreply.github.com>
2022-08-06 22:03:02 +0500

    Initial Commit

---
+++ Rock_Paper_Scissors.py
@@ -0,0 +1,24 @@
+import random
+
+choices = ['rock', 'paper', 'scissors']
+randchoice = (random.choice(choices))
+print(randchoice)
+
+
+def play():
+    ''' This is play Game function '''
+    userInput = input("Enter rock,paper and scissors: ")
+    if userInput == randchoice:
+        print('You Won')
+    else:
+        print("You lost")
+
+
+if __name__ == "__main__":
+    while (True):
+        inp = input("Enter Command 'play' to play or press any key to exit: ")
+        if inp == 'play':
+            play()
+        else:
+            print("Thank you for playing Game")
+            break
